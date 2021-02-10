752ba4fc0ec9a794aa2a7aeb1f0372048e114a1a
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2021-02-10 18:59:27 +0100

    Added resize in noise addition

--- dataset.py
+++ dataset.py
@@ -53,6 +53,9 @@ class ImageDataset(Dataset):
         image_path = self.image_paths[idx]
         clean_image = np.array(Image.open(image_path)) / 255
         noisy_image = clean_image.copy()
+        
+        noisy_image = cv2.resize(noisy_image, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
+        
         noisy_image = noise.pepper(
             noisy_image, amount=random.uniform(self.p_min, self.p_max)
         )
@@ -63,6 +66,8 @@ class ImageDataset(Dataset):
             noisy_image, amount=random.uniform(self.s_min, self.s_max)
         )
 
+        noisy_image = cv2.resize(noisy_image, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+        
         clean_image = clean_image.astype(np.float32)
         noisy_image = noisy_image.astype(np.float32)
 
